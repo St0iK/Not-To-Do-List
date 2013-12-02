f904f5eaa8bfc9248933beb09087b3daf3dc60bf
Dimitris Stoikidis <jstoikidis@gmail.com>
2013-12-02 20:25:05 +0000

    showing all users & updating user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-
-	def show
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+	
+  def index
+     @users = User.paginate(page: params[:page])
+  end
+  def show
 		@user = User.find(params[:id])
 	end
 	def new
@@ -18,12 +23,39 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #succesfull update
+      flash[:success] = "Your account has been updated"
+      redirect_to @user
+    else
+      #not updated
+      render 'edit'
+    end
+    
+  end
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+ def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 
